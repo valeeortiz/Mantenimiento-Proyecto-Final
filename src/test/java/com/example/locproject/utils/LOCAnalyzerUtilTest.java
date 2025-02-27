f6b0ab2b82d7205d4c1b2ed5e8a2361beb88be85
MiguelCastilloSanchez <A18003592@alumnos.uady.mx>
2025-02-26 22:11:22 -0600

    Added second unit test

---
+++ src/test/java/com/example/locproject/utils/LOCAnalyzerUtilTest.java
@@ -0,0 +1,38 @@
+package com.example.locproject.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+class LOCAnalyzerUtilTest {
+
+    private LOCAnalyzerUtil locAnalyzerUtil;
+
+    @BeforeEach
+    void setUp() {
+        locAnalyzerUtil = new LOCAnalyzerUtil();
+        locAnalyzerUtil.reset();
+    }
+
+    @Test
+    void testCountLinesOfCode() throws IOException {
+        File testFile = Paths.get("src", "test", "resources", "JavaTestFile.txt").toFile();
+
+        locAnalyzerUtil.countLinesOfCode(testFile);
+
+        String expectedOutput =
+                "Program: JavaTestFile.txt\n" +
+                "Logical Lines = 44 | Physical Lines = 83 | Total Lines = 93" +
+                "\n----------------------------------------------------------";
+
+        assertEquals(expectedOutput, locAnalyzerUtil.getResult().trim(),
+                "Test didn't pass");
+    }
+
+    
+}
